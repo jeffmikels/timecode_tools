2143e822d22e31ec5840af2d48529ec13aa88864
Jeff Mikels <jeffweb@mikels.cc>
2018-12-07 00:12:06 -0500

    midi_listener changes

--- midi_listener.py
+++ midi_listener.py
@@ -16,13 +16,20 @@ def listen(port_name):
 		print(msg)
 
 @click.command()
-@click.option('--port', '-p', help='name of MIDI port to connect to')
-def main(port):
-	if (port is None):
+@click.option('--name', '-n', help='name of MIDI port to connect to')
+@click.option('--port', '-p', type=int, help='number of MIDI port to connect to, defaults to 0')
+def main(port, name):
+	ports = mido.get_output_names()
+	if (port is None and name is None):
 		print ('Available MIDI ports')
-		print (mido.get_output_names())
+		index = -1
+		for name in ports:
+			index+=1
+			print (f'{index} : {name}')
+	elif not name is None:
+		listen(name)
 	else:
-		listen(port)
+		listen(ports[port])
 		
 
 main()
